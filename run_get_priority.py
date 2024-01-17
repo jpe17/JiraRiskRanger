4a22d30a4a9f232f81de35a854c61efff422ac5a
joaoesteves-laptop <jpe17@ic.ac.uk>
2024-01-17 15:55:01 +0000

    updates

--- run_get_priority.py
+++ run_get_priority.py
@@ -10,46 +10,65 @@ import io
 load_dotenv()
 
 # Streamlit app title
-st.title('JIRA RISK CALCULATOR')
+st.title('JIRA PRIORITY RANK')
 
-# Streamlit input for JQL query
-jql_query = st.text_input('Enter your JQL query',
-                          'project = OS AND statusCategory = 2 AND status != "Blocked By Dev" AND "Module[Dropdown]" '
-                          '= SA AND "Priority Rank[Number]" is not EMPTY AND "Region[Dropdown]" in (EMEA) ORDER BY '
-                          'cf[10169] ASC')
+# Define JQL queries for each region
+jql_queries = {
+    "EMEA": 'project = OS AND statusCategory = 2 AND status != "Blocked By Dev" AND "Module[Dropdown]" = SA AND "Priority Rank[Number]" is not EMPTY AND "Region[Dropdown]" in (EMEA) ORDER BY cf[10169] ASC',
+    "APAC": 'project = OS AND statusCategory = 2 AND "Module[Dropdown]" = SA AND "Region[Dropdown]" in ("APAC++") AND "Priority Rank[Number]" is not EMPTY ORDER BY cf[10169] ASC',
+    "NA": 'project = OS AND statusCategory = 2 AND "Module[Dropdown]" = SA AND "Priority Rank[Number]" is not EMPTY AND "Region[Dropdown]" in ("North America") AND "Customer Phase[Dropdown]" in (Ongoing, Onboarding, Prospect) ORDER BY cf[10169] ASC',
+    "LATAM": 'project = OS AND statusCategory = 2 AND "Module[Dropdown]" = SA AND "Priority Rank[Number]" is not EMPTY AND "Region[Dropdown]" in (latam) ORDER BY cf[10169] ASC',
+    "Global": 'project = OS AND (labels != backlog OR labels is EMPTY) AND statusCategory = "To Do" AND (Module=SA OR type=Mission) AND "Priority Rank[Number]"!=EMPTY ORDER BY cf[10169] ASC, priority DESC, due ASC, updated ASC, created DESC'
+}
 
-# Section for fetching data from JIRA
-if st.button('Fetch Data from JIRA'):
-    jira = JiraFetch()
-    fields = ['*all']
-    tickets = jira.search_issues(jql_query, fields=fields, expand='changelog')
-    jira.write_issues_to_json(tickets, file_name='To_Do_jira_tickets.json')
-    entry_count = count_json_entries('To_Do_jira_tickets.json')
-    st.write(f"Imported {entry_count} issues successfully!")
+# Initialize session state for selected JQL query and region name
+if 'selected_jql' not in st.session_state:
+    st.session_state.selected_jql = None
+if 'selected_region' not in st.session_state:
+    st.session_state.selected_region = None
 
-# Section for calculating risk
-if st.button('Calculate Risk'):
-    jira_calc = JiraCalculateRisk('To_Do_jira_tickets.json', 'jira_sf_mapping.json', 'estimated_times.csv')
-    report_df = jira_calc.get_prio_report_df()
+# Horizontal layout for region buttons
+cols = st.columns(5)
+regions = list(jql_queries.keys())
+for i, col in enumerate(cols):
+    if col.button(regions[i]):
+        st.session_state.selected_jql = jql_queries[regions[i]]
+        st.session_state.selected_region = regions[i]
 
-    # Set pandas options to display all columns
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.float_format', '{:.1f}'.format)
+# Check if a region is selected
+if st.session_state.selected_jql and st.session_state.selected_region:
+    st.write(f"You have selected the region: {st.session_state.selected_region}")
 
-    # Display the DataFrame in Streamlit
-    st.dataframe(report_df)
+    # Section for fetching data from JIRA
+    if st.button('Fetch Data from JIRA'):
+        jira = JiraFetch()
+        fields = ['*all']
+        tickets = jira.search_issues(st.session_state.selected_jql, fields=fields, expand='changelog')
+        jira.write_issues_to_json(tickets, file_name='To_Do_jira_tickets.json')
+        entry_count = count_json_entries('To_Do_jira_tickets.json')
+        st.write(f"Imported {entry_count} issues successfully!")
 
-    # Convert DataFrame to CSV
-    csv = report_df.to_csv(index=False)
-    # To convert to a CSV file in memory
-    b = io.BytesIO()
-    b.write(csv.encode())
-    b.seek(0)
+    # Section for calculating risk
+    if st.button('Calculate Risk'):
+        jira_calc = JiraCalculateRisk('To_Do_jira_tickets.json', 'jira_sf_mapping.json', 'estimated_times.csv')
+        report_df = jira_calc.get_prio_report_df()
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.float_format', '{:.1f}'.format)
 
-    # Create a download button and make the CSV file downloadable
-    st.download_button(
-        label="Download report as CSV",
-        data=b,
-        file_name='report.csv',
-        mime='text/csv',
-    )
+        # Display the DataFrame in Streamlit
+        st.dataframe(report_df)
+
+        # Convert DataFrame to CSV
+        csv = report_df.to_csv(index=False)
+        # To convert to a CSV file in memory
+        b = io.BytesIO()
+        b.write(csv.encode())
+        b.seek(0)
+
+        # Create a download button and make the CSV file downloadable
+        st.download_button(
+            label="Download report as CSV",
+            data=b,
+            file_name='report.csv',
+            mime='text/csv',
+        )
